a93567e44ffd8b2e10ea9b11410956e05a0286c2
OlgaMatykina <89583270+OlgaMatykina@users.noreply.github.com>
2023-12-08 01:28:07 +0300

    Add registration

--- app/models.py
+++ app/models.py
@@ -3,8 +3,13 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from app import login
 
-class User(db.Model):
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
@@ -17,6 +22,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Experiment(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -26,7 +37,11 @@ class Experiment(db.Model):
     user_id: so.Mapped[str] = so.mapped_column(sa.ForeignKey(User.id),
                                                index=True)
 
-    author: so.Mapped[User] = so.relationship('User', back_populates='posts')
+    author: so.Mapped[User] = so.relationship('User', back_populates='experiments')
 
     def __repr__(self):
         return '<Experiment {}>'.format(self.body)
+    
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
