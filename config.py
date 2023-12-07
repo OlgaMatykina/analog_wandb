061169132c1add3688d6b956d3e7c4b94ef1d935
OlgaMatykina <89583270+OlgaMatykina@users.noreply.github.com>
2023-12-08 00:25:38 +0300

    Add experiment table

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
-CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-OPENID_PROVIDERS = [
+class Config(object):
+    # ...
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
